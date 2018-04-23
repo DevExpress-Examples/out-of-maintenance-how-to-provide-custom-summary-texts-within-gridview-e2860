b1e0486fa4973f3deadbe4498080922b6aecce66
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/GridView.Summary/Controllers/GridViewController.vb
@@ -0,0 +1,23 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Web
+Imports System.Web.Mvc
+Imports GridView.Summary.Models
+
+Namespace GridView.Summary.Controllers
+	<HandleError> _
+	Public Class GridViewController
+		Inherits Controller
+		Public Function Index() As ActionResult
+			Return Summary()
+		End Function
+		Public Function Summary() As ActionResult
+			Return View("Summary", NorthwindDataProvider.GetInvoices())
+		End Function
+		Public Function SummaryPartial() As ActionResult
+			Return PartialView("SummaryPartial", NorthwindDataProvider.GetInvoices())
+		End Function
+	End Class
+End Namespace
